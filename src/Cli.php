8208eef3533f9274bfaae91a3e366e3d672cde58
Faik-man <faik199417@gmail.com>
2026-04-22 21:47:29 +0300

    Реализовано приветствие через автозагрузку скрипта из директории src

---
+++ src/Cli.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BrainGames\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function welcomeUser(): void
+{
+    line("Welcome to the Brain Game!\n");
+
+    $name = prompt("May I have your name?\n");
+    line("Hello, %s!\n", $name);
+}
