2144d9f7f3c1ed813af514dcc4aef736f7217002
Faik-man <faik199417@gmail.com>
2026-04-27 19:22:05 +0300

    Убраны лишние функции в Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -9,66 +9,39 @@ use function cli\prompt;
 
 const MAX_ATTEMPTS = 3;
 
-function welcomeUser(): string
-{
-    line("Welcome to the Brain Games!");
-
-    $name = prompt("May I have your name?", false, ' ');
-    line("Hello, %s!", $name);
-
-    return $name;
-}
-
-function askQuestion(string $question): void
-{
-    line('Question: %s', $question);
-}
-
-function makeAnswer(): string
-{
-    return prompt("Your answer");
-}
-
-function congratulateUser(string $name): void
-{
-    line("Congratulations, %s!", $name);
-}
-
-function tryAgain(string $userAnswer, string $correctAnswer, string $name): void
-{
-    line(
-        "'%s' is wrong answer ;(. Correct answer was '%s'.",
-        $userAnswer,
-        $correctAnswer
-    );
-
-    line("Let's try again, %s!", $name);
-}
-
 function gameLoop(
     string $textGoal,
     callable $generatorQuestion,
     callable $generatorCorrectAnswer,
     callable $checkerAnswer
 ): void {
-    $name = welcomeUser();
+    line("Welcome to the Brain Games!");
+
+    $name = prompt("May I have your name?", false, ' ');
+    line("Hello, %s!", $name);
 
     line($textGoal);
 
     for ($i = 0; $i < MAX_ATTEMPTS; $i++) {
         $question = $generatorQuestion();
-        askQuestion($question);
+        line('Question: %s', $question);
 
-        $userAnswer = makeAnswer();
+        $userAnswer = prompt("Your answer");
 
         $correctAnswer = $generatorCorrectAnswer($question);
         if (!$checkerAnswer($userAnswer, $correctAnswer)) {
-            tryAgain($userAnswer, $correctAnswer, $name);
+            line(
+                "'%s' is wrong answer ;(. Correct answer was '%s'.",
+                $userAnswer,
+                $correctAnswer
+            );
+
+            line("Let's try again, %s!", $name);
             return;
         }
 
         line("Correct!");
     }
 
-    congratulateUser($name);
+    line("Congratulations, %s!", $name);
 }
